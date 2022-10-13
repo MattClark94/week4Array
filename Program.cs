304b865693da3ef23a978e2c75cd63d6cd9aaf09
Matt.Clark <mc1556@student.aru.ac.uk>
2022-10-13 14:06:16 +0100

    hg

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[,] addressBook = new string[10,2]; 
+
+
+for (int i=0; i<10; i++)
+{ 
+    Console.WriteLine("Enter Name"); 
+    addressBook[i,0] = Console.ReadLine(); 
+    Console.WriteLine("Enter Address"); 
+    addressBook[i,1] = Console.ReadLine();  
+}
+
+Console.WriteLine("Name       |           Address")
+
+
+Console.ReadKey(); 
